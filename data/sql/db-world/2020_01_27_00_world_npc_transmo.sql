b2b6d77cd96385bf1b00cd5fb82d26474284b0db
jackhein <moustiquejunior@gmail.com>
2024-01-15 16:26:49 +0100

    add npc in Silvermoon & Exodar

---
+++ data/sql/db-world/2020_01_27_00_world_npc_transmo.sql
@@ -0,0 +1,27 @@
+INSERT INTO `creature`
+(
+`id1`,
+`map`,
+`zoneId`,
+`areaId`,
+`spawnMask`,
+`phaseMask`,
+`equipment_id`,
+`position_x`,
+`position_y`,
+`position_z`,
+`orientation`,
+`spawntimesecs`,
+`wander_distance`,
+`currentwaypoint`,
+`curhealth`,
+`curmana`,
+`MovementType`,
+`npcflag`,
+`unit_flags`,
+`dynamicflags`,
+`ScriptName`,
+`VerifiedBuild`)
+VALUES
+(190010,530,0,0,1,1,0,9632.56,-7153.52,14.3241,4.03709,300,0,0,16379,0,0,0,0,0,'',0),
+(190010,530,0,0,1,1,0,-4002.08,-11737.4,-151.624,2.7214,300,0,0,16379,0,0,0,0,0,'',0);
