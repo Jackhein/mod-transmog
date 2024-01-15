f7e61a222ecde426585fadb94f82be33f4438b55
jackhein <moustiquejunior@gmail.com>
2024-01-15 16:20:00 +0100

    insert ignore duplication for transfer of transmo

--- data/sql/db-characters/2024_01_13_00_transmo_migration_tc_to_ac.sql
+++ data/sql/db-characters/2024_01_13_00_transmo_migration_tc_to_ac.sql
@@ -1,2 +1,2 @@
-INSERT INTO `custom_transmogrification` (`GUID`, `FakeEntry`, `Owner`) select `guid`, `transmog`, `owner_guid` from `item_instance` where `transmog` != 0 and `owner_guid` != 0;
+INSERT IGNORE INTO `custom_transmogrification` (`GUID`, `FakeEntry`, `Owner`) select `guid`, `transmog`, `owner_guid` from `item_instance` where `transmog` != 0 and `owner_guid` != 0;
 ALTER TABLE item_instance DROP COLUMN transmog;
