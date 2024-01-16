c51971f8ced00c57cc9cfa7297a78d869d90bb7b
jackhein <moustiquejunior@gmail.com>
2024-01-16 14:17:39 +0100

    Check if column exist

--- data/sql/db-characters/2024_01_13_00_transmo_migration_tc_to_ac.sql
+++ data/sql/db-characters/2024_01_13_00_transmo_migration_tc_to_ac.sql
@@ -1,2 +1,26 @@
-INSERT IGNORE INTO `custom_transmogrification` (`GUID`, `FakeEntry`, `Owner`) select `guid`, `transmog`, `owner_guid` from `item_instance` where `transmog` != 0 and `owner_guid` != 0;
-ALTER TABLE item_instance DROP COLUMN transmog;
+DELIMITER //
+
+CREATE PROCEDURE transmogrification_migration_tc_to_ac()
+BEGIN
+    DECLARE column_count INT;
+
+    -- Check if transmog column exists
+    SELECT COUNT(*)
+    INTO column_count
+    FROM information_schema.columns
+    WHERE table_name = 'item_instance'
+    AND column_name = 'transmog';
+
+    -- If the column exists, perform migration and deletion
+    IF column_count > 0 THEN
+        -- Copy previous transmog
+	INSERT IGNORE INTO `custom_transmogrification` (`GUID`, `FakeEntry`, `Owner`) select `guid`, `transmog`, `owner_guid` from `item_instance` where `transmog` != 0 and `owner_guid` != 0;
+
+        -- Remove old transmog column
+	ALTER TABLE item_instance DROP COLUMN transmog;
+    END IF;
+END //
+
+DELIMITER ;
+
+CALL transmogrification_migration_tc_to_ac();
